124e2bd2543d930727220bb77006d53aa6f1a601
microcai <microcaicai@gmail.com>
2014-12-31 18:19:59 +0800

    add qt stub

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tianya
+
+tianya 是一个专门看 tianya 小说的神器. 当然, 也有下载小说(自动发到 kindle)的神奇功能.
+
+
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+
+#include <thread>
+#include <QApplication>
+
+#include <tianya_list.hpp>
+
+class AsioQApplication : public QApplication
+{
+public:
+	AsioQApplication(int& argc, char* argv[])
+		: QApplication(argc, argv)
+	{
+		m_asio_thread = std::thread([this]()
+		{
+			m_work.reset(new boost::asio::io_service::work(m_asio));
+			m_asio.run();
+		});
+
+		connect(this, &QApplication::aboutToQuit, this, [this]()
+		{
+			m_asio.post([this](){m_work.reset();});
+		});
+	}
+
+	boost::asio::io_service& get_io_service(){return m_asio;}
+
+	~AsioQApplication()
+	{
+		m_asio_thread.join();
+	}
+
+private:
+	std::thread m_asio_thread;
+	boost::asio::io_service m_asio;
+	std::unique_ptr<boost::asio::io_service::work> m_work;
+};
+
+int main(int argc, char* argv[])
+{
+	AsioQApplication app(argc, argv);
+
+	return app.exec();
+}
