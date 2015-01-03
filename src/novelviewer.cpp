0854828c393922a546d09bf3533e2a480067fb1e
microcai <microcaicai@gmail.com>
2015-01-03 12:16:13 +0800

    ui update

--- src/novelviewer.cpp
+++ src/novelviewer.cpp
@@ -1,18 +1,52 @@
 ﻿
 #include <fstream>
 
+#include <QAction>
 #include <QTimer>
 #include <QtWidgets>
 #include <QFileDialog>
 
 #include "novelviewer.hpp"
 
+class QMenuButton : public QWidgetAction
+{
+	QAbstractButton* m_button;
+public:
+    explicit QMenuButton(QAbstractButton* button, QObject* parent)
+		: QWidgetAction(parent)
+	{
+		(m_button = button);
+
+		setDefaultWidget(m_button);
+	}
+
+    virtual ~QMenuButton()
+	{
+//		if (m_button)
+//			m_button->deleteLater();
+	}
+
+    virtual QWidget* createWidget(QWidget* parent)
+	{
+		parent->layout()->addWidget(m_button);
+//		m_button->setParent(parent);
+		auto r = m_button;
+		m_button = NULL;
+		return r;
+	}
+
+};
+
 NovelViewer::NovelViewer(boost::asio::io_service& io, list_info info, QWidget *parent)
 	: QMainWindow(parent)
 	, m_io_service(io)
-	, m_tianya_download(io, info)
+	, m_tianya_download(io, info, true)
 {
 	ui.setupUi(this);
+
+	m_toolbar = new QToolBar(this);
+
+	addToolBar(Qt::TopToolBarArea, m_toolbar);
 
 	m_title = info.title;
 
@@ -23,26 +57,47 @@ NovelViewer::NovelViewer(boost::asio::io_service& io, list_info info, QWidget *p
 	ui.textBrowser->clear();
 
 	connect(&m_tianya_download, SIGNAL(chunk_download_notify(QString)), ui.textBrowser, SLOT(append(QString)));
+
+
+	connect(&m_tianya_download, &tianya_download::chunk_download_notify, this, [this](QString)
+	{
+		statusBar()->showMessage(QStringLiteral("下载中..."));
+	});
+
+
  	connect(&m_tianya_download, SIGNAL(timed_first_timershot()), this, SLOT(text_brower_stay_on_top()));
 
 	connect(&m_tianya_download, SIGNAL(download_complete()), this, SLOT(download_complete()));
 
 	m_tianya_download.start();
 
-	m_action_send_to_kindkle = menuBar()->addAction(QStringLiteral("发送到 kindle(&K)"));
-	m_action_save_to_file = menuBar()->addAction(QStringLiteral("保存文件(&S)"));
+	m_action_send_to_kindkle = m_toolbar->addAction(QStringLiteral("发送到 kindle(&K)"));
+	m_action_send_to_kindkle->setShortcuts({QKeySequence("Ctrl+K"), QKeySequence("Alt+K")});
+	m_toolbar->addSeparator();
 
-	m_action_save_to_file->setShortcut(QKeySequence("Ctrl+S"));
+	m_action_save_to_file = m_toolbar->addAction(QStringLiteral("保存文件(&S)"));
+	m_action_save_to_file->setShortcuts({QKeySequence("Ctrl+S"), QKeySequence("Alt+S")});
 
+	m_toolbar->addSeparator();
+
+	auto cbox = new QCheckBox(QStringLiteral("过滤楼主回复贴(&R)"));
+	//auto cbox = m_toolbar->addAction(QStringLiteral("过滤楼主回复贴(&R)"));
+
+	cbox->setCheckable(true);
+	cbox->setChecked(m_tianya_download.filter_reply());
+
+	connect(cbox, SIGNAL(clicked(bool)), &m_tianya_download, SLOT(set_filter_reply(bool)));
+
+	m_toolbar->addWidget(cbox);
 
 	connect(m_action_send_to_kindkle, &QAction::hovered, this, [this]()
 	{
-		statusBar()->showMessage(QStringLiteral("将当前文章发送到 Kindle 设备"));
+		statusBar()->showMessage(QStringLiteral("将当前文章发送到 Kindle 设备"), 1);
 	});
 
 	connect(m_action_save_to_file, &QAction::hovered, this, [this]()
 	{
-		statusBar()->showMessage(QStringLiteral("将当前文章保存到文件"));
+		statusBar()->showMessage(QStringLiteral("将当前文章保存到文件"), 1);
 	});
 
 	connect(m_action_save_to_file, SIGNAL(triggered(bool)), this, SLOT(save_to()));
@@ -52,6 +107,16 @@ NovelViewer::NovelViewer(boost::asio::io_service& io, list_info info, QWidget *p
 
 NovelViewer::~NovelViewer()
 {
+}
+
+bool NovelViewer::filter_reply()
+{
+	return m_tianya_download.property("filter_reply").toBool();
+}
+
+void NovelViewer::set_filter_reply(bool v)
+{
+	m_tianya_download.setProperty("filter_reply", v);
 }
 
 void NovelViewer::changeEvent(QEvent *e)
